db335add062943df87d5136cadad70fc83c8f478
james oppel <james@haixiao.io>
2021-05-15 02:49:00 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+//! Pong Tutorial 1
+
+use amethyst::{
+    prelude::*,
+    utils::application_root_dir,
+    core::transform::TransformBundle,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    input::{
+        InputBundle,
+        StringBindings,
+    },
+};
+
+mod pong;
+use crate::pong::Pong;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle = InputBundle::<StringBindings>::new()
+        .with_bindings_from_file(binding_path)?;
+    
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                .with_plugin(RenderFlat2D::default()),
+            )?
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        ;
+
+    let assets_dir = app_root.join("assets");
+
+    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    game.run();
+
+    Ok(())
+}
+
